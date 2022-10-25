559e127e51e907d69c2bd8a71d8cf4e5373c7ece
pedro <pedro@PC-MAIN>
2022-10-24 19:37:38 -0700

    Papure inicializadores de varibles

---
+++ src/mx/itson/kuruma/ui/MainTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mx.itson.kuruma.ui;
+
+/**
+ *
+ * @author pedro
+ */
+public class MainTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String moneda[] = {"euro", "dolar"};
+        double cambio[] = {0.1500, 1.00};
+        
+        for (int i = 0; i == i; i++) {
+            System.out.println("Nombre de moneda: "+moneda[i]+"\t"+cambio[i]);
+        }
+    }
+    
+}
